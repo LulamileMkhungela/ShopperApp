5e2eb0f4ff77ab727edf46a911924bf0ae8127ab
Le_Loux Lulamile <mkhungela.l@gmail.com>
2020-11-26 15:52:32 -0800

    updated the business side of the app

---
+++ app/src/main/java/com/hosinc/shopperapp/shopper/app/BsYouSellPostView.java
@@ -0,0 +1,14 @@
+package com.hosinc.shopperapp.shopper.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BsYouSellPostView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bs_you_sell_post_view);
+    }
+}
